a389247e772b3c927ccff3717ff7e6789f111b11
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-09 15:42:14 -0500

    added more test

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class Student {
@@ -7,16 +8,19 @@ public class Student {
     private ArrayList<Integer> grades;
 
     public Student(String name){
+
         this.name = name;
         this.grades = new ArrayList<>();
     }
 
-    // returns the student's name
-    public String getName()
-    {
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
         return name;
-    };
-    // adds the given grade to the grades property
+    }
+
     public void addGrade(int grade){
         grades.add(grade);
     }
@@ -24,13 +28,13 @@ public class Student {
     public ArrayList<Integer> getGrade(){
         return grades;
     }
-    // returns the average of the students grades
+
     public double getGradeAverage(){
         double sum = 0;
-        for(Integer grade : grades)
-        {
+        for(Integer grade : grades) {
             sum += grade;
         }
         return sum / grades.size();
     }
+
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
 
+    Student studentOne = new Student("Johnny");
+    Student studentTwo = new Student("Joanna");
+    Student studentThree = new Student("Travis");
+
+    @Test
+    public void testGetStudentId(){
+        assertNotNull(studentOne);
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Johnny", studentOne.getName());
+        assertEquals("Joanna", studentTwo.getName());
+        assertEquals("Travis", studentThree.getName());
+    }
+
+    @Test
+    public void testAddStudentGrade(){
+        studentOne.addGrade(100);
+        assertEquals(1, studentOne.getGrade().size());
+    }
+
+    @Test
+    public void testAverageStudentGrade(){
+        studentOne.addGrade(100);
+        studentOne.addGrade(88);
+        assertEquals(94, studentOne.getGradeAverage());
+    }
 
 }
