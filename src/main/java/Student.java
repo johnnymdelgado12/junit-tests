7a91b1d194a539189fce3a8957e36fec4228b7e9
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-09 13:58:34 -0500

    first commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName()
+    {
+        return name;
+    };
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrade(){
+        return grades;
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for(Integer grade : grades)
+        {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+}
